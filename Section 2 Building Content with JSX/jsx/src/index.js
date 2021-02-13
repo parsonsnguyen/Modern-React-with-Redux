d7d91e8152a71d2ddfec226bf70bc9043b3ef26d
Parsons Nguyen <parsons.nguyen@nexcel.vn>
2021-02-13 13:42:38 +0700

    Section 2 Building Content with JSX.

---
+++ Section 2 Building Content with JSX/jsx/src/index.js
@@ -0,0 +1,23 @@
+// Import the React and ReactDOM libraries
+import React from "react";
+import ReactDOM from "react-dom";
+
+// Create a react component
+const App = () => {
+  const buttonText = { text: "Click me" };
+  const labelText = "Enter name:";
+  const stylying = { backgroundColor: "blue", color: "white" };
+
+  return (
+    <div>
+      <label className="label" htmlFor="name">
+        {labelText}
+      </label>
+      <input id="name" type="text" />
+      <button style={stylying}>{buttonText.text}</button>
+    </div>
+  );
+};
+
+// Take the react component and show it on the screen
+ReactDOM.render(<App />, document.querySelector("#root"));
